16f533069de25fe4b5f1f2d7edd34a97c44cf0ab
Mateusz Russak <mateusz@russak.biz>
2025-01-04 18:47:08 +0100

    fix: channel info not loading

--- app/src/js/store/methods/search.ts
+++ app/src/js/store/methods/search.ts
@@ -12,7 +12,7 @@ export const find = createMethod('search/find', async ({ channelId, text }: Quer
   await dispatch(methods.users.init());
   const state: StateType = getState();
   if(!state.channels[channelId]){
-    await dispatch(methods.channels.find({ id: channelId }));
+    await dispatch(methods.channels.find(channelId));
   }
   const preprocess = async (m: Message[]) => decryptMessage(m, channelId, state);
   if(state.channels[channelId]?.channelType === 'DIRECT') {
